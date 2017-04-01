00e16ddb9a636ea6bf1035c3648101330c35e4f5
sam65536 <sam19@ukr.net>
2017-04-01 13:50:56 +0300

    fix formatting

--- src/main/java/com/geekhub/security/MvcConfig.java
+++ src/main/java/com/geekhub/security/MvcConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
-	@Override
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
     }
 }
