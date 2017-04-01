65b79c0b426915363c368b64e56059017ccc170a
sam65536 <sam19@ukr.net>
2017-04-01 12:27:09 +0300

    fix formatting

--- src/main/java/com/geekhub/domain/Image.java
+++ src/main/java/com/geekhub/domain/Image.java
@@ -13,46 +13,46 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Image {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-	private Date insertionDate;
-	private String path;
+    private Date insertionDate;
+    private String path;
 
-	@JsonBackReference
-	@ManyToOne
-	private Hotel hotel;
+    @JsonBackReference
+    @ManyToOne
+    private Hotel hotel;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public Date getInsertionDate() {
-		return insertionDate;
-	}
+    public Date getInsertionDate() {
+        return insertionDate;
+    }
 
-	public void setInsertionDate(Date insertionDate) {
-		this.insertionDate = insertionDate;
-	}
+    public void setInsertionDate(Date insertionDate) {
+        this.insertionDate = insertionDate;
+    }
 
-	public String getPath() {
-		return path;
-	}
+    public String getPath() {
+        return path;
+    }
 
-	public void setPath(String path) {
-		this.path = path;
-	}
+    public void setPath(String path) {
+        this.path = path;
+    }
 
-	public Hotel getHotel() {
-		return hotel;
-	}
+    public Hotel getHotel() {
+        return hotel;
+    }
 
-	public void setHotel(Hotel hotel) {
-		this.hotel = hotel;
-	}
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
