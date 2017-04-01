15e4f709f10a59159f1a8b1c0ec0923e948a55ef
sam65536 <sam19@ukr.net>
2017-04-01 19:35:21 +0300

    some fixes & formatting(controllers)

--- src/main/java/com/geekhub/controllers/ApplicationController.java
+++ src/main/java/com/geekhub/controllers/ApplicationController.java
@@ -2,7 +2,6 @@ package com.geekhub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,57 +15,56 @@ import com.geekhub.security.AllowedForCommentModerator;
 @Controller
 public class ApplicationController {
 
-	@Autowired
-	HotelRepository hotels;
-	
-	@Autowired
-	UserRepository users;
+	private final HotelRepository hotels;
+	private final UserRepository users;
 
-	@RequestMapping(value="/")
-	public String root(Model model) {
-		model.addAttribute("hotels", hotels.findAll());
-		return "landing-page";
+    @Autowired
+    public ApplicationController(HotelRepository hotels, UserRepository users) {
+        this.hotels = hotels;
+        this.users = users;
+    }
+
+    @RequestMapping(value = "/")
+    public String root(Model model) {
+        model.addAttribute("hotels", hotels.findAll());
+        return "landing-page";
 	}
 	
-	@RequestMapping(value="/angular")
-	public String angular() {		
-		return "angular-interface";
-	}
+	@RequestMapping(value = "/angular")
+    public String angular() {
+        return "angular-interface";
+    }
 	
-	@RequestMapping(value="/signedin")
-	public String signedIn(Model model, Authentication authentication) {
-		
-		CustomUserDetail principal = (authentication != null) ? (CustomUserDetail) authentication.getPrincipal() : null;
-
-		if (principal != null) {
-			String a = ((SimpleGrantedAuthority) principal.getAuthorities().iterator().next()).getAuthority();
-			if (a.equals(("ROLE_ADMIN")))
-				return "redirect:/admin";
-			else if (a.equals("ROLE_COMMENT_MODERATOR"))
-				return "redirect:/comments/moderation";
-			else if (a.equals(("ROLE_USER")))
-				return "redirect:/users/me";
-			else if (a.equals("ROLE_HOTEL_MANAGER"))
-				return "redirect:/users/me";
+	@RequestMapping(value = "/signedin")
+    public String signedIn(Model model, Authentication authentication) {
+        CustomUserDetail principal = (authentication != null) ? (CustomUserDetail) authentication.getPrincipal() : null;
+        if (principal != null) {
+            String authority = principal.getAuthorities().iterator().next().getAuthority();
+		    if ( authority.equals(("ROLE_ADMIN")) ) {
+		        return "redirect:/admin";
+            } else if (authority.equals("ROLE_COMMENT_MODERATOR")) {
+		        return "redirect:/comments/moderation";
+            } else if (authority.equals(("ROLE_USER"))) {
+                return "redirect:/users/me";
+            } else if (authority.equals("ROLE_HOTEL_MANAGER")) {
+                return "redirect:/users/me";
+            }
 		}
-		
-		return "/"; // fallback
-	}
+        return "/";
+    }
 	
-	@RequestMapping(value="/comments/moderation")
-	@AllowedForCommentModerator
-	public String moderateComments(Model model)
-	{
-		model.addAttribute("hotels", hotels.findAll());
-		return "comments/comment-moderating";
-	}
+    @RequestMapping(value = "/comments/moderation")
+    @AllowedForCommentModerator
+    public String moderateComments(Model model) {
+        model.addAttribute("hotels", hotels.findAll());
+        return "comments/comment-moderating";
+    }
 	
-	@RequestMapping(value="/admin")
-	@AllowedForAdmin
-	public String manageUsers(Model model)
-	{
-		model.addAttribute("users", users.findAll());
-		model.addAttribute("hotels", hotels.findAll());
-		return "admin-dashboard";
-	}
+    @RequestMapping(value="/admin")
+    @AllowedForAdmin
+    public String manageUsers(Model model) {
+        model.addAttribute("users", users.findAll());
+        model.addAttribute("hotels", hotels.findAll());
+        return "admin-dashboard";
+    }
 }
