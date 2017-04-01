d85dc25b43da88233871d4fe2ea4641862ad7a76
sam65536 <sam19@ukr.net>
2017-04-01 12:10:43 +0300

    fix formatting

--- src/main/java/com/geekhub/domain/CustomUserDetail.java
+++ src/main/java/com/geekhub/domain/CustomUserDetail.java
@@ -8,53 +8,53 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 public class CustomUserDetail implements UserDetails {
 
-	private User user;
-	private Set<GrantedAuthority> authorities = null;
+    private User user;
+    private Set<GrantedAuthority> authorities = null;
 
-	public User getUser() {
-	return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-	return authorities;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
 
-	public void setAuthorities(Set<GrantedAuthority> authorities) {
-		this.authorities=authorities;
-	}
+    public void setAuthorities(Set<GrantedAuthority> authorities) {
+        this.authorities=authorities;
+    }
 
-	@Override
-	public String getPassword() {
-		return user.getPassword();
-	}
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
 
-	@Override
-	public String getUsername() {
-		return user.getUsername();
-	}
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
