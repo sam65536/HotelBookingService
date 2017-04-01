9ba2722befeed8f909b876cb0ca9309e66c59ee0
sam65536 <sam19@ukr.net>
2017-04-01 13:10:27 +0300

    fix formatting

--- src/main/java/com/geekhub/domain/RoomType.java
+++ src/main/java/com/geekhub/domain/RoomType.java
@@ -8,39 +8,39 @@ import javax.persistence.Id;
 @Entity
 public class RoomType {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-	private String description;
-	private String occupancy;
+    private String description;
+    private String occupancy;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getOccupancy() {
-		return occupancy;
-	}
+    public String getOccupancy() {
+        return occupancy;
+    }
 
-	public void setOccupancy(String occupancy) {
-		this.occupancy = occupancy;
-	}
+    public void setOccupancy(String occupancy) {
+        this.occupancy = occupancy;
+    }
 
-	@Override
-	public String toString() {
-		return description;
-	}
+    @Override
+    public String toString() {
+        return description;
+    }
 }
