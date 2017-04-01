b95d21d72bfed347f5f1aff741f82f0e04efbdea
sam65536 <sam19@ukr.net>
2017-04-01 11:43:21 +0300

    some fixes & formatting(domain, security, repositories, exceptions, service)

--- src/main/java/com/geekhub/domain/Category.java
+++ src/main/java/com/geekhub/domain/Category.java
@@ -8,32 +8,32 @@ import javax.persistence.Id;
 @Entity
 public class Category {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 	
-	private String name;
+    private String name;
 
-	public Category() {
-	}
+    public Category() {
+    }
 
-	public Category(String name) {
-		this.name = name;
-	}
+    public Category(String name) {
+        this.name = name;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 }
