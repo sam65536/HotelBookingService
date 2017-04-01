83fb7d020b93a43b79c45a52af83dacc20fdbbdb
sam65536 <sam19@ukr.net>
2017-04-01 19:44:42 +0300

    some fixes & formatting(controllers)

--- src/main/java/com/geekhub/controllers/ApplicationController.java
+++ src/main/java/com/geekhub/controllers/ApplicationController.java
@@ -15,8 +15,8 @@ import com.geekhub.security.AllowedForCommentModerator;
 @Controller
 public class ApplicationController {
 
-	private final HotelRepository hotels;
-	private final UserRepository users;
+    private final HotelRepository hotels;
+    private final UserRepository users;
 
     @Autowired
     public ApplicationController(HotelRepository hotels, UserRepository users) {
@@ -24,47 +24,47 @@ public class ApplicationController {
         this.users = users;
     }
 
-    @RequestMapping(value = "/")
-    public String root(Model model) {
-        model.addAttribute("hotels", hotels.findAll());
-        return "landing-page";
-	}
+   @RequestMapping(value = "/")
+   public String root(Model model) {
+       model.addAttribute("hotels", hotels.findAll());
+       return "landing-page";
+   }
 	
-	@RequestMapping(value = "/angular")
-    public String angular() {
-        return "angular-interface";
+   @RequestMapping(value = "/angular")
+   public String angular() {
+       return "angular-interface";
+   }
+	
+   @RequestMapping(value = "/signedin")
+   public String signedIn(Model model, Authentication authentication) {
+       CustomUserDetail principal = (authentication != null) ? (CustomUserDetail) authentication.getPrincipal() : null;
+       if (principal != null) {
+           String authority = principal.getAuthorities().iterator().next().getAuthority();
+		   if ( authority.equals(("ROLE_ADMIN")) ) {
+		       return "redirect:/admin";
+           } else if (authority.equals("ROLE_COMMENT_MODERATOR")) {
+		       return "redirect:/comments/moderation";
+           } else if (authority.equals(("ROLE_USER"))) {
+               return "redirect:/users/me";
+           } else if (authority.equals("ROLE_HOTEL_MANAGER")) {
+               return "redirect:/users/me";
+           }
+	   }
+       return "/";
     }
 	
-	@RequestMapping(value = "/signedin")
-    public String signedIn(Model model, Authentication authentication) {
-        CustomUserDetail principal = (authentication != null) ? (CustomUserDetail) authentication.getPrincipal() : null;
-        if (principal != null) {
-            String authority = principal.getAuthorities().iterator().next().getAuthority();
-		    if ( authority.equals(("ROLE_ADMIN")) ) {
-		        return "redirect:/admin";
-            } else if (authority.equals("ROLE_COMMENT_MODERATOR")) {
-		        return "redirect:/comments/moderation";
-            } else if (authority.equals(("ROLE_USER"))) {
-                return "redirect:/users/me";
-            } else if (authority.equals("ROLE_HOTEL_MANAGER")) {
-                return "redirect:/users/me";
-            }
-		}
-        return "/";
-    }
+   @RequestMapping(value = "/comments/moderation")
+   @AllowedForCommentModerator
+   public String moderateComments(Model model) {
+       model.addAttribute("hotels", hotels.findAll());
+       return "comments/comment-moderating";
+   }
 	
-    @RequestMapping(value = "/comments/moderation")
-    @AllowedForCommentModerator
-    public String moderateComments(Model model) {
-        model.addAttribute("hotels", hotels.findAll());
-        return "comments/comment-moderating";
-    }
-	
-    @RequestMapping(value="/admin")
-    @AllowedForAdmin
-    public String manageUsers(Model model) {
-        model.addAttribute("users", users.findAll());
-        model.addAttribute("hotels", hotels.findAll());
-        return "admin-dashboard";
-    }
+   @RequestMapping(value="/admin")
+   @AllowedForAdmin
+   public String manageUsers(Model model) {
+       model.addAttribute("users", users.findAll());
+       model.addAttribute("hotels", hotels.findAll());
+       return "admin-dashboard";
+   }
 }
