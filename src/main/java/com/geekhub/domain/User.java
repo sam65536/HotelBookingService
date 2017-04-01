6b940ab125ebc1fc2e0a36e9dd3faf19fd008512
sam65536 <sam19@ukr.net>
2017-04-01 13:23:50 +0300

    fix formatting

--- src/main/java/com/geekhub/domain/User.java
+++ src/main/java/com/geekhub/domain/User.java
@@ -19,101 +19,101 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String email;
+    private String name;
+    private String password;
+
+    @Column(unique = true)
+    private String username;
+
+    @ManyToOne
+    private Authority authority;
+
+    @JsonManagedReference
+    @OneToMany(fetch = FetchType.EAGER, mappedBy="user", orphanRemoval = true)
+    @MapKeyColumn(name="id")
+    private Map<Long, Comment> comments = new HashMap<>();
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @JsonManagedReference
+    @OneToMany(fetch = FetchType.EAGER, mappedBy="manager", orphanRemoval = true)
+    private Set<Hotel> hotels = new HashSet<>();
 
-	private String email;
-	private String name;
-	private String password;
+    public User() {
+    }
 
-	@Column(unique = true)
-	private String username;
+    public User(String email, String name, String password, String username) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.username = username;
+    }
 
-	@ManyToOne
-	private Authority authority;
+    public Iterable<Comment> getComments() {
+        return comments.values();
+    }
 
-	@JsonManagedReference
-	@OneToMany(fetch = FetchType.EAGER, mappedBy="user", orphanRemoval = true)
-	@MapKeyColumn(name="id")
-	private Map<Long, Comment> comments = new HashMap<>();
-	
-	@JsonManagedReference
-	@OneToMany(fetch = FetchType.EAGER, mappedBy="manager", orphanRemoval = true)
-	private Set<Hotel> hotels = new HashSet<>();
+    public long getId() {
+        return id;
+    }
 
-	public User() {
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public User(String email, String name, String password, String username) {
-		this.email = email;
-		this.name = name;
-		this.password = password;
-		this.username = username;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public Iterable<Comment> getComments() {
-		return comments.values();
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getEmail() {
-		return email;
-	}
-	
-	public Set<Hotel> getHotels() {
-		return hotels;
-	}
-	
-	public long getId() {
-		return id;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public void setComments(Map<Long, Comment> comments) {
-		this.comments = comments;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setHotels(Set<Hotel> hotels) {
-		this.hotels = hotels;
-	}
+    public Authority getAuthority() {
+        return authority;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setComments(Map<Long, Comment> comments) {
+        this.comments = comments;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public Set<Hotel> getHotels() {
+        return hotels;
+    }
 
-	public Authority getAuthority() {
-		return authority;
-	}
-
-	public void setAuthority(Authority authority) {
-		this.authority = authority;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setHotels(Set<Hotel> hotels) {
+        this.hotels = hotels;
+    }
 }
