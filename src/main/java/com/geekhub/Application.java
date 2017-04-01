23b2ff0e4318ac0964bae9f41afc3e60cc36c817
sam65536 <sam19@ukr.net>
2017-04-01 14:43:23 +0300

    fix formatting

--- src/main/java/com/geekhub/Application.java
+++ src/main/java/com/geekhub/Application.java
@@ -42,32 +42,32 @@ public class Application implements CommandLineRunner {
 
    @Override
    public void run(String... strings) {
-	   users.findAll().forEach(user -> {
-		   String password = user.getPassword();
-		   user.setPassword(SecurityConfig.encoder.encode(password));
-		   users.save(user);
-	   });
+	  users.findAll().forEach(user -> {
+		  String password = user.getPassword();
+		  user.setPassword(SecurityConfig.encoder.encode(password));
+		  users.save(user);
+	  });
 
-	   for (Booking booking : bookings.findAll()) {
-		   Date begin = booking.getBeginDate();
-		   Date end = booking.getEndDate();
-		   List<Date> dates = new ArrayList<>();
-		   Calendar calendar = new GregorianCalendar();
-		   calendar.setTime(begin);
-		   while (calendar.getTime().getTime() <= end.getTime()) {
-			   Date result = calendar.getTime();
-			   dates.add(result);
-			   calendar.add(Calendar.DATE, 1);
-		   }
-		   Map<Date, Long> tmpMap = new HashMap<>();
-		   for (Date date : dates) {
-			   tmpMap.put(date, booking.getId());
-		   }
-		   for (Room room : booking.getRooms()) {
-			   room.setReservedDays(tmpMap);
-			   rooms.save(room);
-		   }
-		   bookings.save(booking);
-	   }
+	  for (Booking booking : bookings.findAll()) {
+		  Date begin = booking.getBeginDate();
+		  Date end = booking.getEndDate();
+		  List<Date> dates = new ArrayList<>();
+		  Calendar calendar = new GregorianCalendar();
+		  calendar.setTime(begin);
+		  while (calendar.getTime().getTime() <= end.getTime()) {
+			  Date result = calendar.getTime();
+			  dates.add(result);
+			  calendar.add(Calendar.DATE, 1);
+		  }
+		  Map<Date, Long> tmpMap = new HashMap<>();
+		  for (Date date : dates) {
+			  tmpMap.put(date, booking.getId());
+		  }
+		  for (Room room : booking.getRooms()) {
+			  room.setReservedDays(tmpMap);
+			  rooms.save(room);
+		  }
+		  bookings.save(booking);
+	  }
    }
 }
