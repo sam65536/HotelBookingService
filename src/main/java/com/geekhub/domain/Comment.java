a13b9891da9900e65cf90f618eff40a88329c2c7
sam65536 <sam19@ukr.net>
2017-04-01 11:59:03 +0300

    fix formatting

--- src/main/java/com/geekhub/domain/Comment.java
+++ src/main/java/com/geekhub/domain/Comment.java
@@ -14,99 +14,99 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Comment {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-	private Date date;
-	private boolean isAnswer;
-	private boolean status;
-	private String text;
+    private Date date;
+    private boolean isAnswer;
+    private boolean status;
+    private String text;
 
-	@OneToOne
-	private Comment reply;
+    @OneToOne
+    private Comment reply;
 		
-	@JsonBackReference
-	@ManyToOne
-	private Hotel hotel;
+    @JsonBackReference
+    @ManyToOne
+    private Hotel hotel;
 	
-	@JsonBackReference
-	@ManyToOne
-	private User user;
+    @JsonBackReference
+    @ManyToOne
+    private User user;
 
-	public Comment() {
-	}
+    public Comment() {
+    }
 
-	public Comment(String text, Date date, User user, boolean status, Hotel hotel) {
-		this.text = text;
-		this.date = date;
-		this.user = user;
-		this.status = status;
-		this.hotel = hotel;
-		this.isAnswer = false;
-	}
+    public Comment(String text, Date date, User user, boolean status, Hotel hotel) {
+        this.text = text;
+        this.date = date;
+        this.user = user;
+        this.status = status;
+        this.hotel = hotel;
+        this.isAnswer = false;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public Date getDate() {
-		return date;
-	}
+    public Date getDate() {
+        return date;
+    }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-	public boolean isAnswer() {
-		return isAnswer;
-	}
+    public boolean isAnswer() {
+        return isAnswer;
+    }
 
-	public void setAnswer(boolean answer) {
-		isAnswer = answer;
-	}
+    public void setAnswer(boolean answer) {
+        isAnswer = answer;
+    }
 
-	public boolean isStatus() {
-		return status;
-	}
+    public boolean isStatus() {
+        return status;
+    }
 
-	public void setStatus(boolean status) {
-		this.status = status;
-	}
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public Comment getReply() {
-		return reply;
-	}
+    public Comment getReply() {
+        return reply;
+    }
 
-	public void setReply(Comment reply) {
-		this.reply = reply;
-	}
+    public void setReply(Comment reply) {
+        this.reply = reply;
+    }
 
-	public Hotel getHotel() {
-		return hotel;
-	}
+    public Hotel getHotel() {
+        return hotel;
+    }
 
-	public void setHotel(Hotel hotel) {
-		this.hotel = hotel;
-	}
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
