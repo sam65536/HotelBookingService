667eba4786901a1310124888f0e7abbbcd229a58
sam65536 <sam19@ukr.net>
2017-04-04 09:21:47 +0300

    some fixes & formatting(controllers)

--- src/main/java/com/geekhub/controllers/HotelController.java
+++ src/main/java/com/geekhub/controllers/HotelController.java
@@ -9,7 +9,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -41,7 +40,6 @@ import com.geekhub.repositories.CategoryRepository;
 import com.geekhub.repositories.CommentRepository;
 import com.geekhub.repositories.HotelRepository;
 import com.geekhub.repositories.ImageRepository;
-import com.geekhub.repositories.RoomRepository;
 import com.geekhub.repositories.RoomTypeRepository;
 import com.geekhub.repositories.UserRepository;
 import com.geekhub.security.AllowedForAdmin;
@@ -50,102 +48,88 @@ import com.geekhub.security.AllowedForManageHotel;
 import com.geekhub.exceptions.HotelNotFoundException;
 
 @Controller
-@RequestMapping(value="/hotels")
+@RequestMapping(value = "/hotels")
 public class HotelController {
 
-	@Autowired
-	HotelRepository hotels;
+	private final HotelRepository hotels;
+	private final CategoryRepository categories;
+	private final RoomTypeRepository roomTypes;
+	private final UserRepository users;
+	private final ImageRepository images;
+	private final CommentRepository comments;
+	private final BookingRepository bookings;
 
 	@Autowired
-	CategoryRepository categories;
+	public HotelController(HotelRepository hotels, CategoryRepository categories, RoomTypeRepository roomTypes, UserRepository users, ImageRepository images, CommentRepository comments, BookingRepository bookings) {
+		this.hotels = hotels;
+		this.categories = categories;
+		this.roomTypes = roomTypes;
+		this.users = users;
+		this.images = images;
+		this.comments = comments;
+		this.bookings = bookings;
+	}
 
-	@Autowired
-	RoomTypeRepository roomTypes;
-
-	@Autowired
-	RoomRepository rooms;
-
-	@Autowired
-	UserRepository users;
-	
-	@Autowired
-	ImageRepository images;
-	
-	@Autowired
-	CommentRepository comments;
-	
-	@Autowired
-	BookingRepository bookings;
-
-	// GET  /hotels 			- the list of hotels
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String index(Model model) {
 		model.addAttribute("hotels", hotels.findAll());
 		return "hotels/index";
 	}
 
-	// GET  /hotels.json 			- the list of hotels
-	@RequestMapping(method=RequestMethod.GET, produces={"text/plain","application/json"})
-	public @ResponseBody Iterable<Hotel> indexJSON(Model model) {
+	@RequestMapping(method = RequestMethod.GET, produces = {"text/plain", "application/json"})
+    public @ResponseBody Iterable<Hotel> indexJSON(Model model) {
 		return hotels.findAll();
 	}
 
-	// GET  /hotels/new			- the form to fill the data for a new hotel
-	@RequestMapping(value="/new", method=RequestMethod.GET)
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
 	@AllowedForHotelManager
-	public String newHotel(Model model) {
-		model.addAttribute("hotel", new Hotel());
-		model.addAttribute("categories", categories.findAll());
-		return "hotels/create";
+    public String newHotel(Model model) {
+	    model.addAttribute("hotel", new Hotel());
+	    model.addAttribute("categories", categories.findAll());
+	    return "hotels/create";
 	}
 
-	// POST /hotels         	- creates a new hotel
-	@RequestMapping(method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST)
 	@AllowedForHotelManager
 	public String saveIt(@ModelAttribute Hotel hotel, Model model) {
-		hotel.setManager(getCurrentUser());
-		hotels.save(hotel);
-		model.addAttribute("hotel", hotel);
-		return "redirect:/hotels";
+	    hotel.setManager(getCurrentUser());
+	    hotels.save(hotel);
+	    model.addAttribute("hotel", hotel);
+	    return "redirect:/hotels";
 	}
 
-	// GET  /hotels/{id} 		- the hotel with identifier {id}
-	@RequestMapping(value="{id}", method=RequestMethod.GET) 	
-	public String show(@PathVariable("id") long id, Model model) {
-		Hotel hotel = hotels.findOne(id);
-		if( hotel == null )
-			throw new HotelNotFoundException();
-		Iterable<Comment> hotel_comments = comments.getComments(id);
-    	
+	@RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") long id, Model model) {
+	    Hotel hotel = hotels.findOne(id);
+	    if (hotel == null) {
+            throw new HotelNotFoundException();
+        }
+		Iterable<Comment> hotelComments = comments.getComments(id);
 		model.addAttribute("booking", new Booking());
-    	model.addAttribute("comments", hotel_comments);
+    	model.addAttribute("comments", hotelComments);
 		model.addAttribute("hotel", hotel );
 		model.addAttribute("reply", new Comment());
 		model.addAttribute("users", users.findAll());
 		model.addAttribute("roomTypes", roomTypes.findAll());
-		
-		Map<Long, Room> rmap = hotel.getRooms();
-		Map<RoomType, Room> rttemp = new HashMap<RoomType, Room>();
-		
-		for(Room r : rmap.values())
-		{
-			rttemp.put(r.getType(), r);
+		Map<Long, Room> rommsMap = hotel.getRooms();
+		Map<RoomType, Room> roomsTypeMap= new HashMap<>();
+		for(Room room : rommsMap.values()) {
+			roomsTypeMap.put(room.getType(), room);
 		}
-		
-		model.addAttribute("hotelRoomTypes", rttemp);		
+		model.addAttribute("hotelRoomTypes", roomsTypeMap);
 		return "hotels/show";
 	}
 
-	// GET  /hotels/{id}.json 		- the hotel with identifier {id}
-	@RequestMapping(value="{id}", method=RequestMethod.GET, produces={"text/plain","application/json"})
+	@RequestMapping(value = "{id}", method = RequestMethod.GET, produces = {"text/plain", "application/json"})
 	public @ResponseBody Hotel showJSON(@PathVariable("id") long id, Model model) {
-		Hotel hotel = hotels.findOne(id);
-		if( hotel == null )
-			throw new HotelNotFoundException();
-		return hotel;
+	    Hotel hotel = hotels.findOne(id);
+		if (hotel == null) {
+		    throw new HotelNotFoundException();
+        }
+        return hotel;
 	}
 
-	@RequestMapping(value="{id}/edit", method=RequestMethod.GET)
+	@RequestMapping(value = "{id}/edit", method = RequestMethod.GET)
 	@AllowedForManageHotel
 	public String edit(@PathVariable("id") long id, Model model) { 	
 		Hotel hotel = hotels.findOne(id);
@@ -155,8 +139,7 @@ public class HotelController {
 		return "hotels/edit";
 	}
 
-	// POST /hotels/{id} 	 	- update the hotel with identifier {id}	
-	@RequestMapping(value="{id}", method=RequestMethod.POST)
+	@RequestMapping(value = "{id}", method = RequestMethod.POST)
 	@AllowedForManageHotel
 	public String editSave(@PathVariable("id") long id, @ModelAttribute("hotel") Hotel hotel) {
 		hotel.setStatus(false);
@@ -164,64 +147,51 @@ public class HotelController {
 		return "redirect:/hotels/{id}";
 	}
 
-	// GET  /hotels/{id}/remove 	- removes the hotel with identifier {id}
-	@RequestMapping(value="{id}/remove", method=RequestMethod.GET)
+	@RequestMapping(value = "{id}/remove", method = RequestMethod.GET)
 	@AllowedForManageHotel
-	public String remove(@PathVariable("id") long id, Model model)
-	{		
-		for(Room r : hotels.findOne(id).getRooms().values())
-		{
-			for(Booking b : r.getBookings())
-			{
-				bookings.delete(b);
+	public String remove(@PathVariable("id") long id, Model model) {
+		for (Room room : hotels.findOne(id).getRooms().values()) {
+			for(Booking booking : room.getBookings()) {
+				bookings.delete(booking);
 			}
 		}
-		
 		hotels.delete(hotels.findOne(id));
 		return "redirect:/hotels";
 	} 
 	
-	@RequestMapping(value="{id}/approve", method=RequestMethod.GET)
+	@RequestMapping(value = "{id}/approve", method = RequestMethod.GET)
 	@AllowedForAdmin
 	public String approve(@PathVariable("id") long id, Model model) {
-		Hotel h = hotels.findOne(id);
-		h.setStatus(true);
-		hotels.save(h);
+		Hotel hotel = hotels.findOne(id);
+		hotel.setStatus(true);
+		hotels.save(hotel);
 		return "redirect:/admin";
 	}  
 
-	@RequestMapping(value="{id}/upload", method=RequestMethod.POST)
+	@RequestMapping(value = "{id}/upload", method = RequestMethod.POST)
 	@AllowedForManageHotel
-	public String uploadImage(@PathVariable("id") long id, Model model,@RequestParam("files") MultipartFile files[] ){
-	
-		if(files.length > 0)
-		{
-			for(int i=0; i < files.length; i++){
-				
+	public String uploadImage(@PathVariable("id") long id, Model model, @RequestParam("files") MultipartFile files[]) {
+		if (files.length > 0) {
+			for (int i = 0; i < files.length; i++) {
 				MultipartFile file = files[i];
 				try {
 					byte[] bytes = file.getBytes();
 					String path = "src/main/resources/public/static/" + file.getOriginalFilename();
-					BufferedOutputStream stream =
-							new BufferedOutputStream(new FileOutputStream(new File(path)));
+					BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(new File(path)));
 					stream.write(bytes);
 					stream.close();
-					
 					Image image = new Image();
 					image.setHotel(hotels.findOne(id));
 					image.setInsertionDate(new Date());
 					image.setPath(file.getOriginalFilename());
 					images.save(image);
-
-				} catch (Exception e) {
-				}
+				} catch (Exception e) {}
 			}
-			return "redirect:/hotels/{id}/upload";
 		}
-		return "";
+		return "redirect:/users/me";
 	}
 	
-	@RequestMapping(value="{id}/upload", method=RequestMethod.GET)
+	@RequestMapping(value = "{id}/upload", method = RequestMethod.GET)
 	@AllowedForManageHotel
 	public String uploadForm(@PathVariable("id") long id, Model model) { 	
 		Hotel hotel = hotels.findOne(id);
@@ -229,92 +199,75 @@ public class HotelController {
 		return "hotels/upload";
 	}
 	
-	@RequestMapping(value="{id}/remove_image/{id_image}", method=RequestMethod.GET)
+	@RequestMapping(value="{id}/remove_image/{imageId}", method = RequestMethod.GET)
 	@AllowedForManageHotel
-	public String deleteImage(@PathVariable("id") long id, @PathVariable("id_image") long id_image, Model model) { 	
-		Image image = images.findOne(id_image);
+	public String deleteImage(@PathVariable("id") long id, @PathVariable("imageId") long imageId, Model model) {
+		Image image = images.findOne(imageId);
 		images.delete(image);
 		return "redirect:/hotels/{id}/upload";
 	}
 	
-	@RequestMapping(value="/search", method=RequestMethod.GET)
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public String searchHotel(Model model, @RequestParam("searchString") String searchString) {
-		Iterator<Hotel> ithotels = hotels.findAll().iterator();
-    	List<Hotel> hotelsList = new ArrayList<Hotel>();	
-    	    	
-    	while(ithotels.hasNext())
-    	{
-    		Hotel h = ithotels.next();
-    		if(h.getName().toLowerCase().contains(searchString.toLowerCase()))
-    			hotelsList.add(h);
-    	}				
-    	
+    	List<Hotel> hotelsList = new ArrayList<>();
+    	for (Hotel hotel : hotels.findAll()) {
+    		if(hotel.getName().toLowerCase().contains(searchString.toLowerCase())) {
+                hotelsList.add(hotel);
+            }
+    	}
     	model.addAttribute("hotels", hotelsList);
     	return "hotels/index";
 	}
 	
-	@RequestMapping(value="{id}/map", method=RequestMethod.POST)
+	@RequestMapping(value = "{id}/map", method = RequestMethod.POST)
 	@AllowedForHotelManager
 	public String hotelMap(@PathVariable("id") long id, Model model, @ModelAttribute Booking booking) {
-		model.addAttribute("begin_date", booking.getBeginDate());
-		model.addAttribute("end_date", booking.getEndDate());
+		model.addAttribute("beginDate", booking.getBeginDate());
+		model.addAttribute("endDate", booking.getEndDate());
 		model.addAttribute("hotel", hotels.findOne(id));		
 		model.addAttribute("occupancy", getOccupancy(hotels.findOne(id), booking.getBeginDate(), booking.getEndDate()));
     	return "hotels/map";
 	}
 
-	public Map<Room, Map<Date, Boolean>> getOccupancy(Hotel hotel, Date begining, Date end) {
-		List<Date> dates = new LinkedList<Date>();
+	private Map<Room, Map<Date, Boolean>> getOccupancy(Hotel hotel, Date begining, Date end) {
+		List<Date> dates = new LinkedList<>();
 		Calendar calendar = new GregorianCalendar();
-		
 		calendar.setTime(begining);
-
-		while (calendar.getTime().getTime() <= end.getTime()){
+		while (calendar.getTime().getTime() <= end.getTime()) {
 			Date result = calendar.getTime();
 			dates.add(result);
 			calendar.add(Calendar.DATE, 1);  
 		}
-		
-		Map<Room, Map<Date, Boolean>> result = new TreeMap<Room, Map<Date, Boolean>>();
-		
-		for (Room r : hotel.getRooms().values())
-		{
-			Map<Date, Long> days_reserved = r.getReservedDays();
-			Map<Date, Boolean> roomOcc = new TreeMap<Date, Boolean>();
-			
-			for (Date d : dates)
-			{
-				SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-				String date = fmt.format(d);
-				
-				for(Date day_reserved : days_reserved.keySet())
-				{
-					String day = fmt.format(day_reserved);
-					
-					if(date.equals(day))
-					{
-						roomOcc.put(d, true);
+		Map<Room, Map<Date, Boolean>> result = new TreeMap<>();
+		for (Room room : hotel.getRooms().values()) {
+			Map<Date, Long> reservedDays = room.getReservedDays();
+			Map<Date, Boolean> roomOccupied = new TreeMap<>();
+			for (Date date : dates) {
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+				String formatDate = simpleDateFormat.format(date);
+				for (Date reservedDate : reservedDays.keySet()) {
+					String day = simpleDateFormat.format(reservedDate);
+					if (formatDate.equals(day)) {
+						roomOccupied.put(date, true);
 					}
-					else
-					{
-						if(!(roomOcc.containsKey(d) && roomOcc.get(d)))
-							roomOcc.put(d, false);
-					}
-				}
-				
-				if(days_reserved.isEmpty())
-					roomOcc.put(d, false);
+					else {
+					    if (!(roomOccupied.containsKey(date) && roomOccupied.get(date))) {
+                            roomOccupied.put(date, false);
+                        }
+                    }
+                }
+				if (reservedDays.isEmpty()) {
+				    roomOccupied.put(date, false);
+                }
 			}
-
-			result.put(r, roomOcc);
+			result.put(room, roomOccupied);
 		}
-		
 		return result;
 	}
 	
 	 private User getCurrentUser() {
-		 Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		 CustomUserDetail myUser= (CustomUserDetail) auth.getPrincipal();
+		 Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		 CustomUserDetail myUser= (CustomUserDetail) authentication.getPrincipal();
 		 return myUser.getUser();
 	 }
 }
--- src/main/java/com/geekhub/domain/Category.java
+++ src/main/java/com/geekhub/domain/Category.java
@@ -36,4 +36,9 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
